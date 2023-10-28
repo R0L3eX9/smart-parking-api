dcce49d196fd7db99eae9dc5cef23882b812d82c
R0L3eX <razvan9rolex@outlook.com>
2023-10-28 16:11:47 +0300

    Updated README

--- README.md
+++ README.md
@@ -1,22 +1,29 @@
 # Smart Parking API
 
+Make sure you have all the required packages:
+```
+pip install -r requirements.txt
+```
+this will install:
+- fastapi
+- uvicorn
+
 Run server locally with:
 ```python
 uvicorn main:app --host 127.0.0.1 --port 8080
 ```
 
-You can run to check:
+You can make a POST request to the endpoint : "/distribution/"
 ```
 curl -X POST http://127.0.0.1:8080/distribution/ \
     -H Accept:application/json \
     -H Content-Type:application/json \
-    -d '{"matrix": [
+    -d '[
                 [-2, -2, -2, -2, -2, -2, -2],
                 [-2, -1, -1, -1, -1,  1, -2, -2],
-                [-2,  2,  0, 0,  0,  0, -1, -2]
-                [-2, -1, -1,  1, -1,  0, -1, -2]
-                [-2, -2, -2, -2, -1,  2, -1, -2]
-                [-2, -2, -2, -2, -2, -2, -2, -2]
-            ]
-        }' \
+                [-2,  2,  0, 0,  0,  0, -1, -2],
+                [-2, -1, -1,  1, -1,  0, -1, -2],
+                [-2, -2, -2, -2, -1,  2, -1, -2],
+                [-2, -2, -2, -2, -2, -2, -2, -2],
+        ]'
 ```
--- main.py
+++ main.py
@@ -7,6 +7,10 @@ class Node:
         self.x = x
         self.y = y
 
+# It creates a 3D array
+# D1 represents the car
+# D2 represents the height
+# D3 represents the width
 def create_dist_array(height, width, cars):
     data = []
     for x in range(cars):
@@ -17,19 +21,20 @@ def create_dist_array(height, width, cars):
                 data[x][i].append(0)
     return data
 
+def get_direction(direction):
+    OX = [-1, 1, 0, 0]
+    OY = [0, 0, -1, 1]
+    return [OX[direction], OY[direction]]
 
+# Finds the only path between the parking spot located at (x, y) and its according car
 def find_path(dist, x, y):
-    di = [-1, 1, 0, 0]
-    dj = [0, 0, -1, 1]
     path = [[x, y]]
-    for d in dist:
-        print(d)
 
     while dist[x][y] != 1:
-        print(dist[x][y])
-        for d in range(0, 4):
-            new_x = x + di[d]
-            new_y = y + dj[d]
+        for direction in range(0, 4):
+            directions = get_direction(direction)
+            new_x = x + directions[0]
+            new_y = y + directions[1]
             if dist[new_x][new_y] == dist[x][y] - 1:
                 x = new_x
                 y = new_y
@@ -40,9 +45,6 @@ def find_path(dist, x, y):
 def find_distribution(encoded_matrix):
     height = len(encoded_matrix)
     width = len(encoded_matrix[0])
-
-    di = [-1, 1, 0, 0]
-    dj = [0, 0, -1, 1]
 
     car_origin = []
     for i in range(0, height):
@@ -66,8 +68,10 @@ def find_distribution(encoded_matrix):
     while len(queue):
         top = queue[0]
         queue = queue[1:]
-        for d in range(0, 4):
-            new_top = Node(top.id, top.x + di[d], top.y + dj[d])
+        for direction in range(0, 4):
+            directions = get_direction(direction)
+            new_top = Node(top.id, top.x + directions[0], top.y + directions[1])
+
             # road
             if encoded_matrix[new_top.x][new_top.y] == 0 and dist[top.id][new_top.x][new_top.y] == 0:
                 dist[top.id][new_top.x][new_top.y] = dist[top.id][top.x][top.y] + 1
@@ -91,18 +95,19 @@ def read_root():
            }
 
 class EncodedMatrix(BaseModel):
-    Matrix: list[list] = [[]]
+    Matrix: list[list]
 
 class Path(BaseModel):
     CarId: int
     Order: list[int]
 
 @app.post("/distribution/")
-async def create_paths(matrix: EncodedMatrix) -> list[Path]:
-    print(matrix)
-    paths = find_distribution(matrix.Matrix)
-    print(paths)
-    result = []
-    for path in paths:
-        result.append(Path(CarId=path[0], Order=path[1]))
-    return result
+async def create_paths(matrix: EncodedMatrix):
+    return matrix
+    # print(matrix)
+    # paths = find_distribution(matrix.Matrix)
+    # print(paths)
+    # result = []
+    # for path in paths:
+    #     result.append(Path(CarId=path[0], Order=path[1]))
+    # return result
